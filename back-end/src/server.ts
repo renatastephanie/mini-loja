bd55b206c49e880a35f852f9ab417fa0d670ea8e
renatastephanie <renatastephaniesantos@gmail.com>
2026-05-29 19:19:52 -0300

    feat: add rota da api

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dontenv from "dotenv";
 import { connectDatabase } from "./config/database";
+import { routes } from "./routes";
 
 dontenv.config();
 
@@ -9,6 +10,7 @@ export const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api', routes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Mini Loja API rodando!" });
