ebb8b45ab21348552055bde148c430055be557d5
renatastephanie <renatastephaniesantos@gmail.com>
2026-06-25 14:15:40 -0300

    fix: adicionado a rota do authRoutes

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { productRoutes } from "./productRoutes";
 import { orderRoutes } from "./orderRoutes";
+import { authRoutes } from "./authRoutes";
 
 export const routes = Router();
 
 routes.use("/products", productRoutes);
 routes.use("/orders", orderRoutes);
+routes.use('/auth', authRoutes)
